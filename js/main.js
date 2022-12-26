959cf73759d2f946652f1517dec08cd0e3f6647a
ZemliakovD <112906798+ZemliakovD@users.noreply.github.com>
2022-12-26 17:57:47 +0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$('.brands-slider').slick({
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1500
+  });
+
+  
+$("#open_window").animatedModal({
+    animatedIn:'zoomIn',
+    animatedOut:'bounceOut',
+    color:'#337AB7'
+});
